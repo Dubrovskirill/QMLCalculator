28b31225666a595140e892b0fe9f775d6d1397ec
Dubrovskirill <zet10set11@gmail.com>
2025-07-07 18:31:13 +0700

    реаизация модели

--- Calculator/CalculatorModel.cpp
+++ Calculator/CalculatorModel.cpp
@@ -1,6 +1,42 @@
-#include "CalculatorModel.h"
+#include "calculatormodel.h"
+#include <stdexcept>
+#include<QSharedPointer>
 
-CalculatorModel::CalculatorModel()
-{
 
+
+CalculatorModel::CalculatorModel(QObject* parent) : QAbstractListModel(parent) {
+    // Инициализация операций (паттерн Strategy)
+    m_operations.insert("+", std::make_shared<AddOperation>());
+    m_operations.insert("-", std::make_shared<SubtractOperation>());
+    m_operations.insert("*", std::make_shared<MultiplyOperation>());
+    m_operations.insert("/", std::make_shared<DivideOperation>());
+}
+
+QHash<int, QByteArray> CalculatorModel::roleNames() const {
+    return {{ExpressionRole, "expression"}, {ResultRole, "result"}};
+}
+
+int CalculatorModel::rowCount(const QModelIndex& parent) const {
+    return parent.isValid() ? 0 : m_history.size();
+}
+
+QVariant CalculatorModel::data(const QModelIndex& index, int role) const {
+    if (!index.isValid() || index.row() >= m_history.size()) return {};
+    const HistoryItem& item = m_history[index.row()];
+    switch (role) {
+    case ExpressionRole: return item.expression;
+    case ResultRole: return item.result;
+    default: return {};
+    }
+}
+
+double CalculatorModel::performOperation(double a, double b, const QString& operation) {
+    if (!m_operations.contains(operation)) throw std::runtime_error("Unknown operation");
+    return m_operations[operation]->execute(a, b);
+}
+
+void CalculatorModel::addToHistory(const QString& expression, double result) {
+    beginInsertRows({}, m_history.size(), m_history.size());
+    m_history.append({expression, result});
+    endInsertRows();
 }
--- Calculator/CalculatorModel.h
+++ Calculator/CalculatorModel.h
@@ -1,11 +1,53 @@
 #ifndef CALCULATORMODEL_H
 #define CALCULATORMODEL_H
+#include <QAbstractListModel>
+#include <QMap>
+#include <QHash>
+#include <functional>
+#include<QSharedPointer>
+
+class IOperationStrategy{
+public:
+    virtual ~IOperationStrategy()=default;
+    virtual double execute(double a, double b) const = 0;
+};
+
+class AddOperation:public IOperationStrategy {
+    double execute(double a, double b) const override {return a+b;}
+};
+
+class SubtractOperation:public IOperationStrategy {
+    double execute(double a, double b) const override {return a-b;}
+};
+
+class MultiplyOperation:public IOperationStrategy {
+    double execute(double a, double b) const override {return a*b;}
+};
+
+class DivideOperation:public IOperationStrategy {
+    double execute(double a, double b) const override {   if (b == 0) throw std::runtime_error("Division by zero"); return a / b;;}
+};
 
 
-class CalculatorModel
+class CalculatorModel : public QAbstractListModel
 {
 public:
-    CalculatorModel();
+   explicit CalculatorModel(QObject* parent = nullptr);
+   enum Roles { ExpressionRole = Qt::UserRole + 1, ResultRole };
+   QHash<int, QByteArray> roleNames() const override;
+   int rowCount(const QModelIndex& parent = {}) const override;
+   QVariant data(const QModelIndex& index, int role) const override;
+
+   Q_INVOKABLE double performOperation(double a, double b, const QString& operation);
+   Q_INVOKABLE void addToHistory(const QString& expression, double result);
+private:
+   struct HistoryItem {
+       QString expression;
+       double result;
+   };
+
+   QList<HistoryItem> m_history;
+   QMap<QString, std::shared_ptr<IOperationStrategy>> m_operations;
 };
 
 #endif // CALCULATORMODEL_H
